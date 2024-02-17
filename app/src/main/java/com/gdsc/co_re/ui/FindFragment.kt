981d184a33fd928e1c9e85ddd77332eae97f9ce3
Yu-jimin <jiminyu0114@gmail.com>
2024-02-17 16:53:26 +0900

    feat: 구글맵 API 불러오기 구현 #9

--- app/src/main/java/com/gdsc/co_re/ui/FindFragment.kt
+++ app/src/main/java/com/gdsc/co_re/ui/FindFragment.kt
@@ -6,13 +6,24 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.gdsc.co_re.R
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
-class FindFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+class FindFragment : Fragment(), OnMapReadyCallback {
+
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var mapView: MapView
+    private lateinit var googleMap: GoogleMap
+    private var currentMarker: Marker? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,7 +37,57 @@ class FindFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_find, container, false)
+        val view = inflater.inflate(R.layout.fragment_find, container, false)
+        mapView = view.findViewById(R.id.mapView)
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
+        return view
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        this.googleMap = googleMap
+        val locations = listOf(
+            LatLng(37.5562, 126.9724) // 서울역
+        )
+        for (location in locations) {
+            googleMap.addMarker(MarkerOptions().position(location))
+        }
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(locations.first(), 10f))
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
     }
 
     companion object {
